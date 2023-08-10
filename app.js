8dd32b6d799b1f0af4fb3c79ddd02a6dc1dd7ca1
barandenizdogan <barandenizdogan@gmail.com>
2023-08-10 22:28:00 +0300

    updates

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const dotenv = require('dotenv');
 const db = require('./config/database.js');
 const Auth = require('./routes/auth.js');
@@ -7,7 +6,6 @@ const Auth = require('./routes/auth.js');
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json({limit:'30mb',extended: true}));
 app.use(express.urlencoded({limit:'30mb',extended: true}));
 
